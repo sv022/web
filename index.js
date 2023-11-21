b01449adbfa589f75fc7a75b6c78662c1400e7bd
sv022 <ssvatkin04@gmail.com>
2023-11-21 12:51:48 +0300

    changes again

---
+++ index.js
@@ -0,0 +1,6 @@
+//let jsonfile = fetch("https://raw.githubusercontent.com/sv022/web/main/files/Epiphone%20Elitist%20Les%20Paul%20Standard%20Ebony/properties.json").then((responce) => responce.json());
+
+function goToListing(listingID){
+    console.log(listingID);
+    
+}
